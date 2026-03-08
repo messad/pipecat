3ffd2e88187d403dffdec69d4539f0d0fc411ceb
EsadD <messad@yandex.ru>
2026-03-08 08:45:03 +0300

    Update server.py

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ import logging
 import base64
 import json
 from typing import Dict, Any, Optional
-from deepgram import LiveTranscriptionOptions as LiveOptions 
+from deepgram import DeepgramClient
 from fastapi import FastAPI, BackgroundTasks, WebSocket, WebSocketDisconnect
 from pydantic import BaseModel
 from loguru import logger
@@ -118,11 +118,12 @@ def service_factory(config: dict):
     stt_model = config.get("stt_model", "nova-3")
     stt_language = config.get("stt_language", "tr")
     stt_sample_rate = config.get("stt_sample_rate", 8000)
+	deepgram_client = DeepgramClient(api_key=os.getenv("DEEPGRAM_API_KEY"))
 
     if stt_provider == "deepgram":
         stt = DeepgramSTTService(
             api_key=os.getenv("DEEPGRAM_API_KEY"),
-            live_options=LiveOptions(
+            live_options=deepgram_client.listen.live.v("1").options(
                 model=stt_model,
                 language=stt_language,
                 encoding="linear16",
